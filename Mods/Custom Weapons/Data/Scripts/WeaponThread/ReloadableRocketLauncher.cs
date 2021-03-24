9c963840a0d130f9782cf2ee12c6678333b46f41
Andy Kipp <kipp.andrew@gmail.com>
2021-03-23 23:06:59 -0700

    Add weaspons core

---
+++ Mods/Custom Weapons/Data/Scripts/WeaponThread/ReloadableRocketLauncher.cs
@@ -0,0 +1,169 @@
+﻿using System.Collections.Generic;
+using static WeaponThread.WeaponStructure;
+using static WeaponThread.WeaponStructure.WeaponDefinition;
+using static WeaponThread.WeaponStructure.WeaponDefinition.HardPointDef;
+using static WeaponThread.WeaponStructure.WeaponDefinition.ModelAssignmentsDef;
+using static WeaponThread.WeaponStructure.WeaponDefinition.HardPointDef.HardwareDef.ArmorState;
+using static WeaponThread.WeaponStructure.WeaponDefinition.HardPointDef.Prediction;
+using static WeaponThread.WeaponStructure.WeaponDefinition.TargetingDef.BlockTypes;
+using static WeaponThread.WeaponStructure.WeaponDefinition.TargetingDef.Threat;
+
+namespace WeaponThread
+{   // Don't edit above this line
+    partial class Weapons
+    {
+        WeaponDefinition SmallRocketLauncherReload => new WeaponDefinition
+        {
+            Assignments = new ModelAssignmentsDef
+            {
+                MountPoints = new[]
+                {
+                    new MountPointDef
+                    {
+                        SubtypeId = "SmallRocketLauncherReload",
+                        AimPartId = "",
+                        MuzzlePartId = "None",
+                        ElevationPartId = "None",
+                        AzimuthPartId = "None",
+                        DurabilityMod = 1f,
+                        IconName = "TestIcon.dds",
+                    },
+
+                },
+                Barrels = new []
+                {
+                    "muzzle_missile_001",
+					"muzzle_missile_002",
+					"muzzle_missile_003",
+					"muzzle_missile_004",
+                },
+            },
+            Targeting = new TargetingDef
+            {
+                Threats = new[]
+                {
+                    Grids, Characters, Projectiles, Meteors, // threats percieved automatically without changing menu settings
+                },
+                SubSystems = new[]
+                {
+                    Thrust, Utility, Offense, Power, Production, Any, // subsystems the gun targets
+                },
+                ClosestFirst = false, // tries to pick closest targets first (blocks on grids, projectiles, etc...).
+                MinimumDiameter = 0, // 0 = unlimited, Minimum radius of threat to engage.
+                MaximumDiameter = 0, // 0 = unlimited, Maximum radius of threat to engage.
+                TopTargets = 4, // 0 = unlimited, max number of top targets to randomize between.
+                TopBlocks = 4, // 0 = unlimited, max number of blocks to randomize between
+                StopTrackingSpeed = 1000, // do not track target threats traveling faster than this speed
+            },
+            HardPoint = new HardPointDef
+            {
+                WeaponName = "SmallRocketLauncherReload", // name of weapon in terminal
+                DeviateShotAngle = 0.1f,
+                AimingTolerance = 4f, // 0 - 180 firing angle
+                AimLeadingPrediction = Advanced, // Off, Basic, Accurate, Advanced
+                DelayCeaseFire = 10, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+
+                Ui = new UiDef
+                {
+                    RateOfFire = false,
+                    DamageModifier = false,
+                    ToggleGuidance = false,
+                    EnableOverload =  false,
+                },
+                Ai = new AiDef
+                {
+                    TrackTargets = false,
+                    TurretAttached = false,
+                    TurretController = false,
+                    PrimaryTracking = false,
+                    LockOnFocus = false,
+                },
+                HardWare = new HardwareDef
+                {
+                    RotateRate = 0.00f,
+                    ElevateRate = 0.00f,
+                    MinAzimuth = 0,
+                    MaxAzimuth = 0,
+                    MinElevation = 0,
+                    MaxElevation = 0,
+                    FixedOffset = false,
+                    InventorySize = 0.24f,
+                    Offset = Vector(x: 0, y: 0, z: 0),
+                },
+                Other = new OtherDef
+                {
+                    GridWeaponCap = 0,
+                    RotateBarrelAxis = 0,
+                    EnergyPriority = 0,
+                    MuzzleCheck = false,
+                    Debug = false,
+                },
+                Loading = new LoadingDef
+                {
+                    RateOfFire = 120,
+                    BarrelSpinRate = 0, // visual only, 0 disables and uses RateOfFire
+                    BarrelsPerShot = 1,
+                    TrajectilesPerBarrel = 1, // Number of Trajectiles per barrel per fire event.
+                    SkipBarrels = 0,
+                    ReloadTime = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    DelayUntilFire = 0, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    HeatPerShot = 10, //heat generated per shot
+                    MaxHeat = 1000000, //max heat before weapon enters cooldown (70% of max heat)
+                    Cooldown = .95f, //percent of max heat to be under to start firing again after overheat accepts .2-.95
+                    HeatSinkRate = 10, //amount of heat lost per second
+                    DegradeRof = false, // progressively lower rate of fire after 80% heat threshold (80% of max heat)
+                    ShotsInBurst = 4,
+                    DelayAfterBurst = 120, // Measured in game ticks (6 = 100ms, 60 = 1 seconds, etc..).
+                    FireFullBurst = false,
+                },
+                Audio = new HardPointAudioDef
+                {
+                    PreFiringSound = "",
+                    FiringSound = "WepShipSmallMissileShot", // subtype name from sbc
+                    FiringSoundPerShot = true,
+                    ReloadSound = "",
+                    NoAmmoSound = "ArcWepShipGatlingNoAmmo",
+                    HardPointRotationSound = "WepTurretGatlingRotate",
+                    BarrelRotationSound = "",
+                },
+                Graphics = new HardPointParticleDef
+                {
+                    Barrel1 = new ParticleDef
+                    {
+                        Name = "", // Smoke_LargeGunShot
+                        Color = Color(red: 1, green: 1, blue: 1, alpha: 1),
+                        Offset = Vector(x: 0, y: -1, z: 0),
+                        Extras = new ParticleOptionDef
+                        {
+                            Loop = false,
+                            Restart = false,
+                            MaxDistance = 200,
+                            MaxDuration = 1,
+                            Scale = 1.0f,
+                        },
+                    },
+                    Barrel2 = new ParticleDef
+                    {
+                        Name = "",//Muzzle_Flash_Large
+                        Color = Color(red: 1, green: 1, blue: 1, alpha: 1),
+                        Offset = Vector(x: 0, y: -1, z: 0),
+                        Extras = new ParticleOptionDef
+                        {
+                            Loop = false,
+                            Restart = false,
+                            MaxDistance = 100,
+                            MaxDuration = 1,
+                            Scale = 1f,
+                        },
+                    },
+                },
+            },
+
+			Ammos = new [] {
+                MissileAmmos
+            },
+            //Animations = AdvancedAnimation,
+            // Don't edit below this line
+        };
+    }
+}
